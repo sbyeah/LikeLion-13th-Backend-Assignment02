b97f714f15e6bb48422b9751053fb68b4e109249
sbyeah <kskb222@gmail.com>
2025-04-13 19:58:28 +0900

    수정_3차..

---
+++ restaurant_assignment/src/test/java/com/likelion/restaurant_assignment/RestaurantAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.restaurant_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
